117309504a2c720af4fbde0c693c870eafbc2c27
Alessandrodev <alessandroadm@live.com>
2021-04-22 00:53:36 -0300

    refactor: change IBaseConnection return and update exports on index

--- domain/shared/repo/base-connection.interface.ts
+++ domain/shared/repo/base-connection.interface.ts
@@ -1,7 +1,7 @@
 import Filter from './filter.interface'
 
 export default interface IBaseConnection<Entity, ORM> {
-     find: (filter: Filter) => Promise<Entity[]>;
+     find: (filter: Filter) => Promise<Entity[] | null>;
      delete: (filter: Filter) => Promise<void>;
      exists: (filter: Filter) => Promise<boolean>;
      save: (target: Entity) => Promise<void>;
