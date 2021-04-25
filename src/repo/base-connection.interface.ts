a1b2b75ecc293b40e5115884bd1174d1d61c512d
Alessandrodev <alessandroadm@live.com>
2021-04-25 11:02:36 -0300

    feat(repository): create abstration to save method On connection must provide two methods create and update and on save method it abstracted all on save method

--- src/repo/base-connection.interface.ts
+++ src/repo/base-connection.interface.ts
@@ -1,10 +1,24 @@
 import Filter from './filter.interface';
 
+/**
+ * `TargetEntity` as Entity to persist on database and
+ * `ORM` as instance or connection or installed ORM
+ * @example UserSchema from infra
+ * @example TypeORM installed instance
+ *
+ * @method find:
+ * @method delete:
+ * @method exists:
+ * @method update:
+ * @method create:
+ * @method orm:
+ */
 export default interface IBaseConnection<Entity, ORM> {
      find: (filter: Filter) => Promise<Entity[] | null>;
      delete: (filter: Filter) => Promise<void>;
      exists: (filter: Filter) => Promise<boolean>;
-     save: (target: Entity) => Promise<void>;
+     update: (target: Entity) => Promise<void>;
+     create: (target: Entity) => Promise<void>;
      orm(): ORM;
 }
 
--- src/repo/base-repository.abstract.ts
+++ src/repo/base-repository.abstract.ts
@@ -2,6 +2,12 @@ import IBaseConnection from './base-connection.interface';
 import Filter from './filter.interface';
 import IMapper from './mapper.interface';
 
+/**
+ * `DomainAggregate` as Aggregate Entity
+ * @example UserAggregate from domain
+ * @example ORM return method types `findOne` `finMany` ...
+ *
+ */
 interface IBaseRepository<DomainAggregate> {
      find: (filter: Filter) => Promise<DomainAggregate[] | null>;
      delete: (filter: Filter) => Promise<void>;
@@ -10,15 +16,39 @@ interface IBaseRepository<DomainAggregate> {
 }
 
 /**
- * Your repository must extends to this one
+ * `Schema` as Entity to persist on database and
+ * `DomainAggregate` as Aggregate Entity
+ * `ORM` as instance or connection or installed ORM
+ * @example UserSchema from infra
+ * @example UserAggregate from domain
+ * @example ORM return method types `findOne` `finMany` ...
+ *
+ * @implements IBaseRepository
+ * @see IBaseRepository
  */
 export default abstract class BaseRepository<DomainAggregate, Entity, ORM>
      implements IBaseRepository<DomainAggregate> {
+     /**
+      *
+      * @param connection instance of ORM connection
+      * @param mapper instance of entity mapper
+      *
+      * @example
+      * ConnectionInterface<TargetPersistence, ORM>,
+      *
+      * @example
+      * MapperInterface<TargetPersistence, DomainAggreate >,
+      */
      constructor(
           protected readonly connection: IBaseConnection<Entity, ORM>,
           protected readonly mapper: IMapper<DomainAggregate, Entity>,
      ) {}
      //
+     /**
+      *
+      * @param filter
+      * @returns DomainAggregate as Array or null
+      */
      async find(filter: Filter): Promise<DomainAggregate[] | null> {
           const targets = await this.connection.find(filter);
           if (!targets) {
@@ -27,21 +57,71 @@ export default abstract class BaseRepository<DomainAggregate, Entity, ORM>
           return targets.map(this.mapper.toDomain);
      }
      //
+     /**
+      *
+      * @param filter
+      * @returns Promise Void
+      */
      async delete(filter: Filter): Promise<void> {
-          await this.connection.delete(filter);
+          const exist = await this.connection.exists(filter);
+          if (exist) {
+               await this.connection.delete(filter);
+          }
      }
      //
+     /**
+      *
+      * @param filter
+      * @returns Promise Boolean. `True` if exists and `False` else
+      */
      async exists(filter: Filter): Promise<boolean> {
           const exist = this.connection.exists(filter);
           return !!exist;
      }
      //
+     /**
+      *
+      * @param target as Domain Aggregate
+      * @returns Promise Void
+      *
+      * @description for this operation is required target must has once of attributes bellow
+      * for identify if is an update or create operation
+      *
+      * @requires `id`
+      *
+      * @requires `_id`
+      */
      async save(target: DomainAggregate): Promise<void> {
           const persistenceValue = this.mapper.toPersistence(target);
-          await this.connection.save(persistenceValue);
+
+          let exist: boolean = false;
+
+          // @ts-expect-error
+          const { id = undefined, _id = undefined } = target;
+
+          // Check if provided id or _id to validate target already exists
+          if (id) {
+               exist = await this.connection.exists({ id });
+          } else if (_id) {
+               exist = await this.connection.exists({ _id });
+          }
+
+          // if target already exist update it else create it
+          if (exist) {
+               await this.connection.update(persistenceValue);
+          } else {
+               await this.connection.create(persistenceValue);
+          }
      }
 }
 
+/**
+ * @extends BaseRepository
+ *
+ * @argument `Schema` as Entity to persist on database and
+ * @argument `DomainAggregate` as Aggregate Entity
+ * @argument `ORM` as instance or connection or installed ORM
+ */
 export interface IRepository<DomainAggregate, Entity, ORM>
      extends BaseRepository<DomainAggregate, Entity, ORM> {}
 
