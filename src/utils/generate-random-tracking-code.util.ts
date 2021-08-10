f4da26dd40bf0b147f4c360500177ee0e118e6ab
4lessandrodev <alessandroadm@live.com>
2021-08-09 23:32:05 -0300

    feat(util): added tracking value object

---
+++ src/utils/generate-random-tracking-code.util.ts
@@ -0,0 +1,44 @@
+const generatePartOne = (): number => {
+	const currentDate = new Date();
+	const startsYear = 1900;
+	const partOne = currentDate.getDate() + (currentDate.getFullYear() - startsYear) + currentDate.getMonth() + currentDate.getDay() + currentDate.getHours() + currentDate.getMinutes();
+	return partOne;
+}
+
+const getRandomChar = (): string => {
+	const alphabet = 'QWERTYUIOPASDFGHJKLZXCVBNM';
+	const randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+	return randomLetter;
+}
+
+const generatePartTwo = (): string => {
+	const currentDate = new Date();
+	const partTwo =	currentDate.getMilliseconds() + currentDate.getMinutes() + Math.floor(Math.random() * 100) + '0000';
+	return partTwo;
+}
+
+const controllerNumber = (): string => {
+	const value = new Date().getTime().toString(16);
+	return value.slice(value.length - 2).toUpperCase();
+}
+
+const generateRandomTracking = (): string => {
+	const partOne = generatePartOne();
+	const partTwo = generatePartTwo();
+	const randomLetter = getRandomChar();
+	const controller = controllerNumber() + getRandomChar();
+	
+	const normalize = partTwo.slice(0, 4);
+	const order = partOne + normalize;
+	
+	let sum = 0;
+	for (let digit = 0; digit < order.length; digit++) {
+		var value = order.slice(digit, digit + 1);
+		sum += parseInt(value);
+	}
+
+	return `${controller}-${randomLetter}${partOne}${sum}-${normalize}`;
+}
+
+export { generateRandomTracking };
+export default generateRandomTracking;
---
+++ src/utils/tracking-code.value-object.ts
@@ -0,0 +1,60 @@
+import { Result, ValueObject } from "..";
+import generateRandomTracking from "./generate-random-tracking-code.util";
+const regexHash = /^[0-9|A-Z]{3}-[\w]{1}[0-9]{5}-[0-9]{4}$/;
+
+interface OrderIdProps {
+	value: string;
+}
+
+class TrackingCodeValueObject extends ValueObject<OrderIdProps> {
+	private constructor(props: OrderIdProps) {
+		super(props);
+	}
+
+	/**
+	 * @returns value as string
+	 */
+	get value(): string {
+		return this.props.value;
+	}
+
+	/**
+	 * 
+	 * @returns tracking code as string
+	 */
+	public static generate(): string {
+		return generateRandomTracking()
+	}
+
+	/**
+	 * 
+	 * @param code value as string
+	 * @returns true if code match with pattern or false if not
+	 * @pattern 
+	 * XXX-A99999-9999
+	 */
+	public static isValidValue(code: string): boolean {
+		return regexHash.test(code);
+	}
+
+	/**
+	 * @description generate a random tracking code.
+	 * The value is optional. If you not provide a new one will be generated.
+	 * @param code optional
+	 * @returns Result of TrackingCode
+	 * @pattern 
+	 * XXX-A99999-9999
+	 */
+	public static create(code?: string): Result<TrackingCodeValueObject> {
+		const value = code ?? TrackingCodeValueObject.generate();
+		if (code) {
+			if (!TrackingCodeValueObject.isValidValue(code)) {
+				return Result.fail<TrackingCodeValueObject>('Invalid value for Tracking code');
+			}
+		}
+		return Result.ok<TrackingCodeValueObject>(new TrackingCodeValueObject({ value }));
+	}
+}
+
+export { TrackingCodeValueObject };
+export default TrackingCodeValueObject;
