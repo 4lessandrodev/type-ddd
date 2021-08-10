ed4c9ea81a3e8630e325eb2d89f15b0487b7d631
4lessandrodev <alessandroadm@live.com>
2021-08-09 23:33:16 -0300

    feat(util): added logger

---
+++ src/utils/logger.util.ts
@@ -0,0 +1,20 @@
+import pino from 'pino';
+
+const config = {
+	prettyPrint: {
+		colorize: true,
+		levelFirst: true,
+		messageFormat: '{levelLabel} {pid} {msg}',
+		translateTime: 'HH:MM:ss',
+		ignore: 'pid,hostname'
+	}
+}
+
+const Logger = {
+	info: (message: string)=> pino(config).info({}, message),
+	error: (message: string) => pino(config).error({}, message),
+	warn: (message: string) => pino(config).warn({}, message),
+}
+
+export { Logger };
+export default Logger;
