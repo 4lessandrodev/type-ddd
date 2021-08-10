e9b41bbf1dc7e2b6304235a1598988721d5a9116
4lessandrodev <alessandroadm@live.com>
2021-08-09 23:35:16 -0300

    test(util): added mobile phone number value object

---
+++ src/utils/tests/mobile-phone.value-object.util.spec.ts
@@ -0,0 +1,53 @@
+import { MobilePhoneValueObject } from '../mobile-phone.value-object';
+
+describe('home-phone.value-object', () => {
+	it('should be defined', ()=>{
+		const valueObject = MobilePhoneValueObject.create;
+		expect(valueObject).toBeDefined();
+	});
+
+	it('should create mobile phone number with success', ()=>{
+		const valueObject = MobilePhoneValueObject.create('(11) 99604-1111');
+		expect(valueObject.isSuccess).toBe(true);
+	})
+
+	it('should fail if try to create an invalid home phone number', ()=>{
+		const valueObject = MobilePhoneValueObject.create('(11) 99999-9999');
+		expect(valueObject.isSuccess).toBe(false);
+	})
+
+	it('should fail if try to create an invalid home phone number', ()=>{
+		const valueObject = MobilePhoneValueObject.create('(11) 11111111');
+		expect(valueObject.isSuccess).toBe(false);
+	})
+
+	it('should fail if try to create an invalid home phone number', ()=>{
+		const valueObject = MobilePhoneValueObject.create('(01) 99061-1111');
+		expect(valueObject.isSuccess).toBe(false);
+	})
+
+	it('should fail if try to create an invalid home phone number', ()=>{
+		const valueObject = MobilePhoneValueObject.create('01 99620-1111');
+		expect(valueObject.isSuccess).toBe(false);
+	})
+
+	it('should fail if try to create an invalid home phone number', ()=>{
+		const valueObject = MobilePhoneValueObject.create('99201-1111');
+		expect(valueObject.isSuccess).toBe(false);
+	})
+
+	it('should get value', ()=>{
+		const valueObject = MobilePhoneValueObject.create('(71) 98254-1211').getResult();
+		expect(valueObject.value).toBe('(71) 98254-1211');
+	})
+
+	it('should get only numbers value', ()=>{
+		const valueObject = MobilePhoneValueObject.create('(71) 96254-1211').getResult();
+		expect(valueObject.getOnlyNumbers()).toBe(71962541211);
+	})
+
+	it('should get only DDD number', ()=>{
+		const valueObject = MobilePhoneValueObject.create('(71) 97254-1211').getResult();
+		expect(valueObject.getDDD()).toBe(71);
+	})
+});
