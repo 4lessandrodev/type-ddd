1727e0077aba2b3f5296e8dafb05434e13b29dd6
4lessandrodev <alessandroadm@live.com>
2021-08-11 02:26:37 -0300

    test(value-object): added test to url value object value object

---
+++ src/utils/tests/url.value-object.util.spec.ts
@@ -0,0 +1,23 @@
+import { UrlValueObject } from '../url.value-object';
+
+describe('url.value-object', () => {
+	it('should be defined', ()=>{
+		const valueObject = UrlValueObject.create;
+		expect(valueObject).toBeDefined();
+	})
+
+	it('should create a valid url', ()=>{
+		const valueObject = UrlValueObject.create('https://google.com');
+		expect(valueObject.isSuccess).toBe(true);
+	})
+
+	it('should create a valid url and get the value', ()=>{
+		const valueObject = UrlValueObject.create('https://google.com').getResult();
+		expect(valueObject.value).toBe('https://google.com');
+	})
+
+	it('should fail if try to create an invalid url', ()=>{
+		const valueObject = UrlValueObject.create('localhost');
+		expect(valueObject.isFailure).toBeTruthy();
+	})
+});
