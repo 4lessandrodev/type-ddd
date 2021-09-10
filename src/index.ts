60988f4acbb4311501adcc24791b9f54427e8492
4lessandrodev <alessandroadm@live.com>
2021-09-09 23:22:14 -0300

    fix(util): export url value object

--- src/index.ts
+++ src/index.ts
@@ -36,3 +36,4 @@ export * from './utils/get-undefined-keys-as-array.util';
 export * from './utils/get-undefined-keys-as-object.util';
 export * from './patterns/specification/specification.composite';
 export * from './utils/pin.value-object';
+export * from './utils/url.value-object';
