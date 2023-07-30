4a8114ef844396971e7c70f7b8465b908df947b9
4lessandrodev <alessandroadm@live.com>
2023-07-30 19:42:52 -0300

    test: added tests to ensure types inference testing to-object method on entity #281

---
+++ tests/entity/product.entity.spec.ts
@@ -0,0 +1,48 @@
+import { Entity, Ok, Result, UID, ValueObject } from 'rich-domain';
+
+describe('entity', () => {
+	describe('toObject', () => {
+		class Name extends ValueObject<{ value: string }> {
+			private constructor(props: { value: string }) {
+				super(props);
+			}
+
+			public static create(value: string): Result<Name> {
+				return Ok(new Name({ value }));
+			}
+		}
+
+		interface Props {
+			id?: UID;
+			price: number;
+			name: Name;
+			additionalInfo: string[];
+			createdAt?: Date;
+			updatedAt?: Date;
+		}
+
+		class Product extends Entity<Props> {
+			private constructor(props: Props) {
+				super(props);
+			}
+			public static create(props: Props): Result<Product> {
+				return Ok(new Product(props));
+			}
+		}
+
+		it('should infer types to aggregate on toObject method', () => {
+			const name = Name.create('orange').value();
+			const props: Props = {
+				name,
+				additionalInfo: ['from brazil'],
+				price: 10,
+			};
+			const orange = Product.create(props).value();
+
+			const object = orange.toObject();
+			expect(object.additionalInfo).toEqual(['from brazil']);
+			expect(object.name).toBe('orange');
+			expect(object.price).toBe(10);
+		});
+	});
+});
